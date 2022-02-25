4a7385067915b577cf0bb05ae6c09829e1983d1b
Yash Deshmukh <19bit171@ietdavv.edu.in>
2022-02-25 09:09:04 +0530

    Blog part done

--- routers/post.js
+++ routers/post.js
@@ -4,18 +4,38 @@ const router = require("express").Router();
 //const { json } = require("body-parser");
 //const { JSON } = require("express/lib/response");
 const JSON = require("JSON");
-const { createPost } = require("../controllers/post");
+const { createPost,deletePost, updatePost , getPost , getFeaturedPosts , getPosts ,searchPost , getRelatedPosts, uploadImage} = require("../controllers/post");
 const multer = require("../middleware/multer");
 const { postvalidator, validate } = require("../middleware/postvalidtor");
 const { parseData } = require("../middleware");
 
-router.post("/create",multer.single("thumbnail"),
-
+router.post(
+    "/create",
+    multer.single("thumbnail"),
     parseData,
+    postvalidator, 
+    validate,
+    createPost
+ );// clear 
 
-postvalidator, 
-validate,
- createPost
- );
+ router.put(
+    "/:postId",
+    multer.single("thumbnail"),
+    parseData,
+    postvalidator, 
+    validate,
+    updatePost
+ );// error hai same as in delete post
+
+
+
+ router.delete("/:postId", deletePost);//error hai iis me 
+ router.get("/single/:slug", getPost);// clear
+ router.get("/featured-posts", getFeaturedPosts);// error 
+router.get("/posts",getPosts);//clear
+router.get("/search",searchPost);// error hai trim me 
+router.get("/related-posts/:postId", getRelatedPosts)// clear 
+
+router.post("/upload-image",multer.single("image"),uploadImage);//clear
 
 module.exports = router;
